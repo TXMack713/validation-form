8f5f7c3995e2a5cc38f42b89b1b571241db8048a
Anthony Mack <mackattack713@gmail.com>
2023-01-19 23:02:40 -0600

    Updated the logic

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ email.addEventListener("input", (event) => {
     email.setCustomValidity("");
   }
 });
+
+const user_occupation = document.getElementById("user_occupation");
+user_occupation.addEventListener("click", getOccupation);
+
+const user_state = document.getElementById("user_state");
+user_state.addEventListener("click", getState);
+
+async function getOccupation() {
+  const url = "https://frontend-take-home.fetchrewards.com/form";
+  const response = await fetch(url);
+  const data = await response.json();
+}
+
+async function getState() {
+  const url = "https://frontend-take-home.fetchrewards.com/form";
+  const response = await fetch(url);
+  const data = await response.json();
+}
