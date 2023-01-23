fdf957c9ff0cf6ccf97486673feb6130d0b8e2d6
TXMack713 <49073356+TXMack713@users.noreply.github.com>
2023-01-23 08:38:27 -0600

    3 files modified

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const email = document.getElementById("user_email");
+const email = document.getElementById('user_email');
 
-email.addEventListener("input", (event) => {
+email.addEventListener('input', (event) => {
   if (email.validity.typeMismatch) {
     email.setCustomValidity(
-      "Please enter a legitimate email to proceed. Ex: myemail@example.com"
+      'Please enter a legitimate email to proceed. Ex: myemail@example.com'
     );
   } else {
-    email.setCustomValidity("");
+    email.setCustomValidity('');
   }
 });
 
-const user_occupation = document.getElementById("user_occupation");
-user_occupation.addEventListener("click", getOccupation);
+const page = document.querySelector('html');
+page.addEventListener('load', getOccupation);
+page.addEventListener('load', getState);
 
-const user_state = document.getElementById("user_state");
-user_state.addEventListener("click", getState);
+const user_occupation = document.getElementById('user_occupation');
+// user_occupation.addEventListener("click", getOccupation);
+
+const user_state = document.getElementById('user_state');
+// user_state.addEventListener("click", getState);
 
 async function getOccupation() {
-  const url = "https://frontend-take-home.fetchrewards.com/form";
+  const url = 'https://frontend-take-home.fetchrewards.com/form';
   const response = await fetch(url);
   const data = await response.json();
-  const occupations = data["occupations"];
+  const occupations = data['occupations'];
   for (let i = 0; i < occupations.length; i++) {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.innerHTML = occupations[i];
     option.value = occupations[i];
     user_occupation.appendChild(option);
@@ -30,15 +34,15 @@ async function getOccupation() {
 }
 
 async function getState() {
-  const url = "https://frontend-take-home.fetchrewards.com/form";
+  const url = 'https://frontend-take-home.fetchrewards.com/form';
   const response = await fetch(url);
   const data = await response.json();
-  const locations = data["states"];
+  const locations = data['states'];
   for (let i = 0; i < locations.length; i++) {
-    const option = document.createElement("option");
-    const locale = locations[i]["abbreviation"] + " - " + locations[i]["name"];
+    const option = document.createElement('option');
+    const locale = locations[i]['abbreviation'] + ' - ' + locations[i]['name'];
     option.innerHTML = locale;
-    option.value = locations[i]["name"];
+    option.value = locations[i]['name'];
     user_state.appendChild(option);
   }
 }
